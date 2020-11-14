2a18db1a04706fc5f342bfd32c871713d2ac8474
Eduardo Diaz <edu.diaz92@gmail.com>
2020-11-14 15:37:31 -0600

    correcciones

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,6 +4,7 @@
 '''
 # %%
 from nltk import data
+from numpy.lib.shape_base import column_stack
 import pandas as pd
 import numpy as np
 import glob
@@ -16,23 +17,28 @@ import unidecode
 import re
 
 import class_words
+import complete_stem_words
 
 # %%
 class Preprocess():
 
-    def __init__(self, rootDir, word_dict):
+    def __init__(self, rootDir, word_dict, inv_words):
         self.rootDir_ = rootDir
         self.class_words_dict_ = word_dict
+        self.inv_words_dict_ = inv_words
         self.spanish_stemmer_ = SnowballStemmer('spanish')
         self.special_words_ = ['piez']
         self.stopwords_spanish_ = stopwords.words('spanish')
-        self.df_ = pd.DataFrame(columns=['Id','Tipo','Tipo_2','Tipo_3','Tipo_4','Marca','Submarca','Empaque','Contenido','UnidadMedida','LocalidadGeografica','Fuente','precio','fecha'])
+        self.df_ = pd.DataFrame(columns=['Tipo','Tipo_2','Tipo_3','Tipo_4','Marca','Submarca','Empaque','Contenido','UnidadMedida','LocalidadGeografica','Fuente','precio','fecha'])
 
         self.data_ = self.import_data()
         self.add_stop_words()
         self.preprocess('descripcion')
         self.categorize()
         self.append_df()
+        self.join_marca_submarca_drop_null()
+        self.imputation()
+        self.inv_words_funct()
 
     def import_data(self):
         '''
@@ -126,7 +132,7 @@ class Preprocess():
         for element in self.data_.keys():
             self.df_ = self.df_.append(self.data_.get(element), ignore_index=True)
         
-        self.df_.drop(['producto','descripcion'], axis=1, inplace=True)
+        self.df_.drop(['producto','descripcion','LocalidadGeografica'], axis=1, inplace=True)
         
         return self
 
@@ -159,13 +165,73 @@ class Preprocess():
                         self.data_.get(base_key)['UnidadMedida'].loc[row] = element
                     
         return self
+    
+    def join_marca_submarca_drop_null(self):
+        self.df_['Marca'] = self.df_['Marca'] + '_' + self.df_['Submarca']
+        self.df_.drop(['Submarca'],axis=1,inplace=True)
+        self.df_.dropna(subset=['Tipo'], inplace=True)
+        
+        return self
+
+    def imputation(self):
+        self.df_.fillna('',inplace=True)
+        self.df_.reset_index(drop=True, inplace=True)
+        for row in range(len(self.df_)):
+            if self.df_.Tipo.loc[row] == 'huev' and self.df_.UnidadMedida.loc[row] == '':
+                self.df_['UnidadMedida'].loc[row] = 'pz'
+            if self.df_.Tipo.loc[row] == 'tortill' and self.df_.UnidadMedida.loc[row] == '':
+                self.df_['UnidadMedida'].loc[row] = 'pz'
+            if self.df_.Tipo.loc[row] == 'papel' and self.df_.UnidadMedida.loc[row] == '':
+                self.df_['UnidadMedida'].loc[row] = 'roll'
+            if self.df_.Contenido.loc[row] == '':
+                self.df_['Contenido'].loc[row] = '1'
+            if self.df_.Marca.loc[row] == '':
+                self.df_['Marca'].loc[row] = 'no_especificado'
+            if self.df_['Tipo_2'].loc[row] == '':
+                if self.df_['Tipo_4'].loc[row] == '' and self.df_['Tipo_3'].loc[row] == '':
+                    self.df_['Tipo_2'].loc[row] = 'no_especificado'
+                else:
+                    if self.df_['Tipo_4'].loc[row] == '':
+                        self.df_['Tipo_2'].loc[row] = self.df_['Tipo_3'].loc[row]
+                    else:
+                        if self.df_['Tipo_3'].loc[row] == '':
+                            self.df_['Tipo_2'].loc[row] = self.df_['Tipo_4'].loc[row]
+                        else:
+                            self.df_['Tipo_2'].loc[row] = self.df_['Tipo_3'].loc[row] + '_' + self.df_['Tipo_4'].loc[row]
+            else:
+                if self.df_['Tipo_4'].loc[row] == '' and self.df_['Tipo_3'].loc[row] == '':
+                    self.df_['Tipo_2'].loc[row] = self.df_['Tipo_2'].loc[row]
+                else:
+                    if self.df_['Tipo_4'].loc[row] == '':
+                        self.df_['Tipo_2'].loc[row] = self.df_['Tipo_2'].loc[row] + '_' + self.df_['Tipo_3'].loc[row]
+                    else:
+                        if self.df_['Tipo_3'].loc[row] == '':
+                            self.df_['Tipo_2'].loc[row] = self.df_['Tipo_2'].loc[row] + '_' + self.df_['Tipo_4'].loc[row]
+                        else:
+                            self.df_['Tipo_2'].loc[row] = self.df_['Tipo_2'].loc[row] + '_' + self.df_['Tipo_3'].loc[row] + '_' + self.df_['Tipo_4'].loc[row]
+        return self
+
+    def search_in_dict(self, data):
+        for key, value in self.inv_words_dict_.items():
+            for i in value:
+                if i == data:
+                    return key
+                else:
+                    pass
+        return data
+    
+    def inv_words_funct(self):
+        column_name = ['Tipo','Tipo_2','Marca','Empaque','UnidadMedida','Contenido']
+        for element in column_name:
+            self.df_[element] = self.df_.apply(lambda row: self.search_in_dict(row[element]), axis=1)
+        return self
 
 # %% 
 rootDir = 'Dataset/'
-clean_class = Preprocess(rootDir=rootDir, word_dict=class_words.class_words_dict)
+clean_class = Preprocess(rootDir=rootDir, word_dict=class_words.class_words_dict, inv_words=complete_stem_words.inv_class_words)
 # %%
 clean_class.df_
 # %%
 clean_class.df_.to_csv('clean_data.csv', index=False)
 # %%
-# Imputar lo que falta con conocimiento general de cada producto.
+clean_class.df_['Tipo_2'].unique()
